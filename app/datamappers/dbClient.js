1e7436b69b6109513793635831300d41404ef240
H-Dev <h-dev@air-de-h.home>
2024-09-02 15:16:21 +0200

    settings postgres heroku

--- app/datamappers/dbClient.js
+++ app/datamappers/dbClient.js
@@ -5,12 +5,12 @@ const { Pool } = pkg;
 // Note: The Pool connection allows the DB to automatically open a new connection for a new user/HTTP request. This means you don't have to wait for the previous SQL query to finish before starting a new one.
 const client = new Pool({
   connectionString: process.env.DATABASE_URL,
-  // max: 20, // Max 20 clientsim the pool
-  // idleTimeoutMillis: 30000, // Close inactive clients after 30 seconds
-  // connectionTimeoutMillis: 2000,
-  // ssl: {
-  //   rejectUnauthorized: false
-  // }
+  max: 20, // Max 20 clientsim the pool
+  idleTimeoutMillis: 30000, // Close inactive clients after 30 seconds
+  connectionTimeoutMillis: 2000,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default client;
