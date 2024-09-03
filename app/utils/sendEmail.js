5109f224e5b48eb18ae341b50c78c3a73e055d1b
H-Dev <h-dev@air-de-h.home>
2024-09-03 10:49:15 +0200

    change settings nodemailer

--- app/utils/sendEmail.js
+++ app/utils/sendEmail.js
@@ -2,13 +2,22 @@ import nodemailer from 'nodemailer';
 
 export const sendVerifyEmail = async (email, token) => {
   try {
+    // const smtpOptions = {
+    //   host: process.env.EMAIL_HOST,
+    //   secure: false,
+    //   port: 587,
+    //   tls: {
+    //     ciphers: 'SSLv3',
+    //   },
+    //   auth: {
+    //     user: process.env.EMAIL_USER,
+    //     pass: process.env.EMAIL_PASSWORD,
+    //   },
+    // };
     const smtpOptions = {
       host: process.env.EMAIL_HOST,
-      secure: false,
-      port: 587,
-      tls: {
-        ciphers: 'SSLv3',
-      },
+      secure: true,
+      port: 465,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD,
