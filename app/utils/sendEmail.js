47561ad57bd205a70ea6d04e38342e52bd752358
h-dev <hugo.marecal@oclock.school>
2024-10-25 17:56:22 +0200

    setting up for deploy on vercel and supabase

--- app/utils/sendEmail.js
+++ app/utils/sendEmail.js
@@ -2,18 +2,6 @@ import nodemailer from 'nodemailer';
 
 export const sendVerifyEmail = async (email, token) => {
   try {
-    // const smtpOptions = {
-    //   host: process.env.EMAIL_HOST,
-    //   secure: false,
-    //   port: 587,
-    //   tls: {
-    //     ciphers: 'SSLv3',
-    //   },
-    //   auth: {
-    //     user: process.env.EMAIL_USER,
-    //     pass: process.env.EMAIL_PASSWORD,
-    //   },
-    // };
     const smtpOptions = {
       host: process.env.EMAIL_HOST,
       secure: true,
@@ -30,10 +18,8 @@ export const sendVerifyEmail = async (email, token) => {
       from: process.env.FROM_EMAIL,
       to: email,
       subject: 'Verify your email',
-      html: `<a href="https://gestinvest-back-39dbe349ad20.herokuapp.com/api/auth/verify/${token}">Click here to verify your email</a>`,
+      html: `<a href="https://gest-invest.vercel.app/api/auth/verify/${token}">Click here to verify your email</a>`,
     });
-
-    console.log('Email envoyé avec succès à', email);
   } catch (error) {
     console.error("Erreur lors de l'envoi de l'email:", error);
   }
