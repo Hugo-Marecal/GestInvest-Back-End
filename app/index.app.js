0e72f0fd1c991fdba524f188d3657c199975e61d
h-dev <hugo.marecal@oclock.school>
2024-11-08 12:00:36 +0100

    uncomment get endpoint for cron

--- app/index.app.js
+++ app/index.app.js
@@ -6,8 +6,8 @@ import cors from 'cors';
 
 // Import CRON
 // import task from './utils/API/CRON.js';
-// import cryptoApi from './utils/API/api.crypto.js';
-// import stockApi from './utils/API/api.stock.js';
+import cryptoApi from './utils/API/api.crypto.js';
+import stockApi from './utils/API/api.stock.js';
 
 // Import main router of the application
 import router from './routers/index.api.router.js';
@@ -31,16 +31,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Route for execute CRON job in vercel
-// app.get('/api/cron', async (req, res) => {
-//   try {
-//     await cryptoApi.getPriceCrypto(1, 60);
-//     await stockApi.getPriceStock(2, 40);
-//     res.status(200).send('Cron job exécuté avec succès.');
-//   } catch (error) {
-//     console.error("Erreur lors de l'exécution du cron job :", error);
-//     res.status(500).send("Erreur lors de l'exécution du cron job.");
-//   }
-// });
+app.get('/api/cron', async (req, res) => {
+  try {
+    await cryptoApi.getPriceCrypto(1, 60);
+    await stockApi.getPriceStock(2, 40);
+    res.status(200).send('Cron job exécuté avec succès.');
+  } catch (error) {
+    console.error("Erreur lors de l'exécution du cron job :", error);
+    res.status(500).send("Erreur lors de l'exécution du cron job.");
+  }
+});
 
 // Use the main router to manage all the routes of the application
 app.use(router);
